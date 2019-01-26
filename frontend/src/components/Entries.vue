5dde18926b4d90d67323b280ba90e0ef1aace75e
Masato Koishi <koishimst@gmail.com>
2019-01-26 12:52:20 +0900

    create transition

--- frontend/src/components/Entries.vue
+++ frontend/src/components/Entries.vue
@@ -2,38 +2,38 @@
   <section class="section">
     <h1 class="title">Hotentry</h1>
 
-    <div class="columns is-multiline">
-
-      <div class="column is-4" v-for="item in items" :key="item.link">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img :src="item.imageurl" alt="image">
-            </figure>
-          </div>
-
-          <div class="card-header">
-            <p class="card-header-title">{{ item.title }}</p>
-          </div>
-
-          <div class="card-content">
-            <div class="content">
-              {{ item.description }}
-            </div>
-            <br>
-            <div class="level">
-              <div class="level-left">
-                <time :datetime="item.datetime">{{ item.datetime }}</time>
+    <transition appear>
+      <div class="columns is-multiline">
+        <div class="column is-4" v-for="item in items" :key="item.link">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img :src="item.imageurl" alt="image">
+                </figure>
               </div>
-              <div class="level-right button is-primary is-rounded">
-                <p>{{ item.bookmarkcount }}</p>
+
+              <div class="card-header">
+                <p class="card-header-title">{{ item.title }}</p>
+              </div>
+
+              <div class="card-content">
+                <div class="content">
+                  {{ item.description }}
+                </div>
+                <br>
+                <div class="level">
+                  <div class="level-left">
+                    <time :datetime="item.datetime">{{ item.datetime }}</time>
+                  </div>
+                  <div class="level-right button is-primary is-rounded">
+                    <p>{{ item.bookmarkcount }}</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-    </div>
+      </transition>
   </section>
 </template>
 
@@ -66,3 +66,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.v-enter-active, .v-leave-active {
+  transition: opacity .5s;
+}
+
+.v-enter, .v-leave-to {
+  opacity: 0;
+}
+</style>
