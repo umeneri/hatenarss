a61042c18112809f28b36d0b07919e4109d0d25b
Masato Koishi <koishimst@gmail.com>
2019-01-26 12:59:53 +0900

    remove unused comment

--- app/controllers/AsyncController.scala
+++ app/controllers/AsyncController.scala
@@ -1,29 +1,14 @@
 package controllers
 
-import javax.inject._
 import akka.actor.ActorSystem
 import hatenarss.services.HatenaRssService
-import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
+import javax.inject._
+import play.api.libs.ws.WSClient
 import play.api.mvc._
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future, Promise}
 
-/**
- * This controller creates an `Action` that demonstrates how to write
- * simple asynchronous code in a controller. It uses a timer to
- * asynchronously delay sending a response for 1 second.
- *
- * @param cc standard controller components
- * @param actorSystem We need the `ActorSystem`'s `Scheduler` to
- * run code after a delay.
- * @param exec We need an `ExecutionContext` to execute our
- * asynchronous code.  When rendering content, you should use Play's
- * default execution context, which is dependency injected.  If you are
- * using blocking operations, such as database or network access, then you should
- * use a different custom execution context that has a thread pool configured for
- * a blocking API.
- */
 @Singleton
 class AsyncController @Inject()(cc: ControllerComponents,
                                 actorSystem: ActorSystem,
@@ -31,14 +16,6 @@ class AsyncController @Inject()(cc: ControllerComponents,
                                (implicit exec: ExecutionContext)
   extends AbstractController(cc) {
 
-  /**
-   * Creates an Action that returns a plain text message after a delay
-   * of 1 second.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/message`.
-   */
   def message: Action[AnyContent] = Action.async {
     getFutureMessage(1.second).map { msg => Ok(msg) }
   }
