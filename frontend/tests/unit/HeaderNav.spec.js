5f61dd9719ca40e40755e2505124043871cc2577
Masato Koishi <koishimst@gmail.com>
2019-03-02 11:53:22 +0900

    add jest test

---
+++ frontend/tests/unit/HeaderNav.spec.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { shallowMount } from '@vue/test-utils'
+import HeaderNav from '@/components/molecules/HeaderNav.vue'
+import { MENU_TYPE } from '@/entities/MenuType'
+
+Vue.use(Router)
+
+describe('HeaderNav.vue', () => {
+  it('toggle menu', () => {
+    const wrapper = shallowMount(HeaderNav, {
+      propsData: { menus: Object.values(MENU_TYPE) }
+    })
+    wrapper.vm.toggleMenu()
+    expect(wrapper.vm.isMenuActive).toBe(true)
+  })
+})
