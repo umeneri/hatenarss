c0f21de544c5648cd69a68906215f8bd50f0fc06
Masato Koishi <koishimst@gmail.com>
2019-04-14 20:59:50 +0900

    add twitter link to about

--- frontend/src/components/pages/About.vue
+++ frontend/src/components/pages/About.vue
@@ -3,6 +3,7 @@
     <div class="hero-body">
       <div class="container has-text-centered">
         <h1>Author: Masato Koishi</h1>
+        <p><a href=https://twitter.com/m_umeneri>@m_umeneri</a></p>
       </div>
     </div>
   </section>
